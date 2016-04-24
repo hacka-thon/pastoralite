8472896f63617c304fc253a056cea2c9b774ef91
Genevieve <gkolve@gmail.com>
2016-04-23 19:47:13 -0700

    [feature] sends 5 day forecast

--- server/apiServices.js
+++ server/apiServices.js
@@ -4,15 +4,28 @@ var key = require('./utils').key;
 
 module.exports = {
 	getWeather: function(query, callback){
-		var queryStr = 'http://api.openweathermap.org/data/2.5/weather?lat=' + query.lat + '&lon=' + query.lon + '&APPID=' + key;
+		var queryStr = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + query.lat + '&lon=' + query.lon + '&APPID=' + key;
 
 		request.get(queryStr, function(err, res, data) {
 			if(err) console.log('ERROR in getData: ', err);
 
 			data = JSON.parse(data);
-			console.log('DATA from getData: ', data);
+			data = data.list;
 
-			callback(data);
+			var dataArr = [];
+
+			for(var i=0 ; i<data.length ; i+=8){
+				var temp = {
+					day: data[i+3],
+					night: data[i+7]
+				}
+
+				dataArr.push(temp);
+			}
+
+			console.log('formated data: ', dataArr)
+
+			callback(dataArr);
 		})
 	},
 	addStation: function(query){
